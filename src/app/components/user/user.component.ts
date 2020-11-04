f4bc51e3498cdb543e3bc43f21861ec6ef92cedc
Dani Solte <danielsoltero667@gmail.com>
2020-11-04 17:46:40 +0100

    Cambiar tu imagen de usuario finalizado

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Data } from '@angular/router';
 import { Router } from '@angular/router';
+import Swal  from 'sweetalert2';
 import * as $ from 'jquery';
 
 import { Usuario } from 'src/usuario';
@@ -34,6 +35,97 @@ export class UserComponent implements OnInit {
     valAtracciones:null,
     comParques:null,
     comAtracciones:null
+  }
+
+  imgMod={
+    nombreArchivo: null, //Nombre de la imagen
+    base64textString: null //Archivo en formato base64
+  }
+
+  modificarFoto(event){
+    //Se obtienen los valores del formulario de modificacion de usuarios
+    const target=event.target;
+    //Se tomara el valor de la etiqueta del formulario para subir archivos
+    let img=target.querySelector('#imgMod').value;
+    if (img==""){//Si no se ha subido ningun archivo nuevo, no se hara nada
+      Swal.fire({
+        title: 'Error al subir una imagen',
+        text: "Selecciona una imagen primero",
+        icon: 'error',
+        confirmButtonColor: '#3085d6',
+        confirmButtonText: 'Volver'
+      })
+    }else{
+      if (this.imgMod.base64textString){//Si se ha subido una nueva imagen
+        //Se llamara al servicio para guardar la nueva imagen en el servidor
+        this.servicioDatos.subirNuevaImagen(this.imgMod).subscribe(datos => {
+          if (datos["result"]=="OK"){//Si el resultado es positivo
+            this.imgMod.nombreArchivo=datos["ruta"];
+            img=this.imgMod.nombreArchivo;//Se obtendra la ruta del archivo como imagen
+            //Se llama al servicio para modificar el usuario
+            this.servicioDatos.modificarUser(this.datosPerfilUsuario.username,this.datosPerfilUsuario.puntos,this.datosPerfilUsuario.tipo,
+                                             img,this.datosPerfilUsuario.username).subscribe(datos => {
+              if (datos["result"]=="OK"){
+                //Si se modifica correctamente el usuario, se modificaran sus datos de la sesion y 
+                //le saltara al usuario un mensaje de informacion
+                this.usuarioConectado.userImg=img;
+                sessionStorage.setItem("usuarioConectado",JSON.stringify(this.usuarioConectado));
+                Swal.fire({
+                  title: 'Se ha modificado tu foto de perfil correctamente',
+                  text: "Refresca la página actualizar los cambios",
+                  icon: 'success',
+                  confirmButtonColor: '#3085d6',
+                  confirmButtonText: 'Refrescar página'
+                }).then((result) => {
+                  if (result.value) {
+                    location.reload();//Si se pulsa el botón, se recargará la página
+                  }
+                })
+              }else{//Si hay algun error al modificar el usuario, saltara un mensaje de error
+                Swal.fire({
+                  title: 'Ha habido un error al modificar tu imagen de perfil',
+                  text: "No se ha podido modificar el usuario",
+                  icon: 'error',
+                  confirmButtonColor: '#3085d6',
+                  confirmButtonText: 'Volver'
+                })
+              }
+            });
+          }else{//Si hay algun error al subir la nueva imagen al servidor, saltara un mensaje de error
+            Swal.fire({
+              title: 'Ha habido un error al subir la imagen al servidor',
+              text: "No se ha podido añadir la nueva imagen",
+              icon: 'error',
+              confirmButtonColor: '#3085d6',
+              confirmButtonText: 'Volver'
+            })
+          }
+        });
+      }
+    }
+  }
+
+  /**
+   * Metodo que se activará cuando se suba un nuevo archivo de imagen
+   * @param event: Evento que se crea al subir una nueva imagen
+   */
+  selectFile(event){
+    //Se obtendrá el archivo del formulario
+    let files=event.target.files;
+    let file=files[0];
+
+    if(files && file) {//Si se ha obtenido un archivo del formulario, se recogera su informacion
+      this.imgMod.nombreArchivo = file.name;
+      var reader = new FileReader();//Se creara un objeto para leer la informacion del archivo
+      reader.onload = this._handleReaderLoaded.bind(this);
+      reader.readAsBinaryString(file);
+    }
+  }
+
+  //Metodo para tratar el archivo de imagen y convertiro en base64
+  _handleReaderLoaded(readerEvent) {
+    var binaryString = readerEvent.target.result;
+    this.imgMod.base64textString = btoa(binaryString);
   }
 
   /**
